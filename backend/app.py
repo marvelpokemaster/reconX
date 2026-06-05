140dd9c6be2c61472da4e35d04b62a94065d0c30
marvelpokemaster <marvelpokemaster@gmail.com>
2026-06-05 19:16:11 +0530

    chore: create tables in flask sql integration psql

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,54 @@
-import time
 from flask import Flask
-import dotenv
-from flask_cors import cors
+import psycopg2
 
 app = Flask(__name__)
-@app.route('/api/time')
 
-def get_current_time():
-    return {'time heyy': time.time()}
-if __name__=="__main__":
-    app.run()
+def init_db():
+    conn = psycopg2.connect(
+        database="reconx",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="5432"
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS investigations (
+            id SERIAL PRIMARY KEY,
+            target VARCHAR(255) NOT NULL,
+            status VARCHAR(20) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            completed_at TIMESTAMP
+        );
+        CREATE TABLE IF NOT EXISTS dns_records (
+            id SERIAL PRIMARY KEY,
+            investigation_id INT NOT NULL,
+            record_type VARCHAR(20) NOT NULL,
+            value TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+            FOREIGN KEY (investigation_id) REFERENCES investigations(id) ON DELETE CASCADE
+        );
+        CREATE TABLE IF NOT EXISTS certificates (
+            id SERIAL PRIMARY KEY,
+            investigation_id INT NOT NULL,
+            issuer VARCHAR(255) NOT NULL,
+            valid_from TIMESTAMP,
+            valid_to TIMESTAMP,
+            FOREIGN KEY (investigation_id) REFERENCES investigations(id) ON DELETE CASCADE
+        );
+        
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+init_db()
+@app.route("/api/")
+def home():
+    return {"message": "ReconX API"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
